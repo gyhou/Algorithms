d60f32f069f82b5a71ad6737fc47cffe4ec0baba
gyhou <47550192+gyhou@users.noreply.github.com>
2019-12-12 22:50:07 -0800

    Add files via upload

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -2,13 +2,23 @@
 
 import sys
 
+
 def rock_paper_scissors(n):
-  pass 
+    rps = ['rock', 'paper', 'scissors']  # The three options
+    num_games = 3 ** n  # How many game will be played
+    # Create list of lists with 0 place holders for each player
+    games = [[0] * n for x in range(num_games)]
+    dividers = [3 ** x for x in range(0, n + 1)]
+    for i in range(0, len(games)):
+        for j in range(1, len(games[0]) + 1):
+            games[i][-j] = rps[(i // dividers[j - 1]) % 3]
+
+    return games
 
 
 if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    num_plays = int(sys.argv[1])
-    print(rock_paper_scissors(num_plays))
-  else:
-    print('Usage: rps.py [num_plays]')
+    if len(sys.argv) > 1:
+        num_plays = int(sys.argv[1])
+        print(rock_paper_scissors(num_plays))
+    else:
+        print('Usage: rps.py [num_plays]')
